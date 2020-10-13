bc8480d8186752afffc1b5bd8e5ac8594280fb80
theodosisathanasakis <th.athanasakis@gmail.com>
2020-10-13 11:02:07 +0300

    add whats app link and minor updates

--- js/scripts.js
+++ js/scripts.js
@@ -9,9 +9,9 @@
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (
-            location.pathname.replace(/^\//, "") ==
+            location.pathname.replace(/^\//, "") ===
                 this.pathname.replace(/^\//, "") &&
-            location.hostname == this.hostname
+            location.hostname === this.hostname
         ) {
             var target = $(this.hash);
             target = target.length
