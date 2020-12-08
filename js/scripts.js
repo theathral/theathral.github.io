6f20d7950c89825aadf2f029aaa9173deecbf8b8
theodosisathanasakis <th.athanasakis@gmail.com>
2020-12-08 20:59:10 +0200

    jquery functions (need fix)

--- js/scripts.js
+++ js/scripts.js
@@ -117,6 +117,15 @@ function score5(id) {
     document.getElementById(id).innerHTML += "<i class='fas fa-circle'></i>";
 }
 
+function score5new() {
+    return "&emsp;"
+        + "<i class='fas fa-circle'></i>"
+        + "<i class='fas fa-circle'></i>"
+        + "<i class='fas fa-circle'></i>"
+        + "<i class='fas fa-circle'></i>"
+        + "<i class='fas fa-circle'></i>";
+}
+
 score4('java');
 score4('sql');
 score3('android');
@@ -142,18 +151,21 @@ score2('linux');
 
 // Work Experience
 function workExperienceDiv(workExp) {
-    let work = JSON.parse(workExp)
-    $('<h3/>').addClass('mb-5').append(work.title.toString()).appendTo('#workExperienceContent');
+    $('<h3/>').addClass('mb-5').append('Work Experience').appendTo('#workExperienceContent');
 
+    workExperienceSingle('');
     // workExp.content.forEach(item => workExperienceSingle(item));
 }
 
 function workExperienceSingle(item) {
     $('<div/>').addClass(['d-flex', 'flex-column', 'flex-column', 'flex-md-row', 'justify-content-between', 'mb-5']).append(
         $('<div/>').addClass('flex-grow-1').append(
-            $('<h4/>').addClass('mb-0').append('Open Sea Lifeguard'),
-            $('<div/>').addClass(['subheading', 'mb-3']).append('BAYLINE SERVICES'),
-            $('<p/>').append('Open Sea Lifeguard at several beaches of Chalkidiki'),
+            $('<h4/>').addClass('mb-0')
+                .append('Open Sea Lifeguard'),
+            $('<div/>').addClass(['subheading', 'mb-3'])
+                .append('BAYLINE SERVICES'),
+            $('<p/>')
+                .append('Open Sea Lifeguard at several beaches of Chalkidiki'),
         ),
         $('<div/>').addClass('flex-shrink-0').append(
             $('<span/>').addClass('text-info').append('September 2020')
@@ -164,9 +176,141 @@ function workExperienceSingle(item) {
 // #Work Experience
 
 
+// Voluntary Experience
+function voluntaryExperienceDiv(workExp) {
+    $('<h3/>').addClass('mb-5').append('Voluntary Experience').appendTo('#voluntaryExperienceContent');
+
+    voluntaryExperienceSingle('');
+    // workExp.content.forEach(item => workExperienceSingle(item));
+}
+
+function voluntaryExperienceSingle(item) {
+    $('<div/>').addClass(['d-flex', 'flex-column', 'flex-column', 'flex-md-row', 'justify-content-between', 'mb-5']).append(
+        $('<div/>').addClass('flex-grow-1').append(
+            $('<h4/>').addClass('mb-0')
+                .append('Reception of Freshmen'),
+            $('<div/>').addClass(['subheading', 'mb-3'])
+                .append('Volunteer Coordination Office, Aristotle University of Thessaloniki'),
+            $('<p/>')
+                .append(''),
+        ),
+        $('<div/>').addClass('flex-shrink-0').append(
+            $('<span/>').addClass('text-info').append('September 2020 - October 2020')
+        )
+    ).appendTo('#voluntaryExperienceContent')
+}
+
+// #Voluntary Experience
 
 
+// Education
+function educationDiv(workExp) {
+    $('<h3/>').addClass('mb-5').append('Education').appendTo('#educationContent');
 
+    educationDivSingle('');
+    // workExp.content.forEach(item => workExperienceSingle(item));
+}
+
+function educationDivSingle(item) {
+    $('<div/>').addClass(['d-flex', 'flex-column', 'flex-column', 'flex-md-row', 'justify-content-between', 'mb-5']).append(
+        $('<div/>').addClass('flex-grow-1').append(
+            $('<h4/>').addClass('mb-0')
+                .append('<i class="fas fa-user-graduate"></i>' + ' ' + 'Computer Science Degree'),
+            $('<div/>').addClass(['subheading', 'mb-3'])
+                .append('Aristotle University of Thessaloniki'),
+            $('<p/>').append(
+                'BSc Thesis during Erasmus Exchange with <strong>Prof. Paris Avgeriou</strong> (University of Groningen) as supervisor.\n'
+                + '<br>'
+                + 'Find more information about my BSc Thesis in "Portfolio" section.'
+            ),
+            $('<ul/>').addClass(['fa-ul', 'mb-3']).append(
+                $('<li/>').append(
+                    $('<span>').addClass('fa-li')
+                        .append('<i class="fas fa-hourglass-half"></i>'),
+                    ' ' + 'Minimum Attendance Duration' + ': ' + '4 Years'
+                ),
+                $('<li/>').append(
+                    $('<span>').addClass('fa-li')
+                        .append('<i class="fas fa-certificate"></i>'),
+                    ' ' + 'Grade' + ': ' + '8.42/10'
+                )
+            )
+        ),
+        $('<div/>').addClass('flex-shrink-0').append(
+            $('<span/>').addClass('text-info').append('October 2016 - November 2020')
+        )
+    ).appendTo('#educationContent')
+}
+
+// #Education
+
+
+// Certifications
+function certificationsDiv(workExp) {
+    $('<h3/>').addClass('mb-5').append('Certifications').appendTo('#certificationsContent');
+
+    certificationsDivSingle('');
+    // workExp.content.forEach(item => workExperienceSingle(item));
+}
+
+function certificationsDivSingle(item) {
+    $('<div/>').addClass(['d-flex', 'flex-column', 'flex-column', 'flex-md-row', 'justify-content-between', 'mb-5']).append(
+        $('<div/>').addClass('flex-grow-1').append(
+            $('<h4/>').addClass('mb-0')
+                .append('<i class="fas fa-user-graduate"></i>' + ' ' + 'Open Water Lifeguard Degree'),
+            $('<div/>').addClass(['subheading', 'mb-3'])
+                .append('BAYLINE SERVICES'),
+            $('<ul/>').addClass(['fa-ul', 'mb-3']).append(
+                $('<li/>').append(
+                    $('<span>').addClass('fa-li')
+                        .append('<i class="fas fa-first-aid">'),
+                    ' ' + 'First Aid Knowledge'
+                ),
+            )
+        ),
+        $('<div/>').addClass('flex-shrink-0').append(
+            $('<span/>').addClass('text-info').append('October 2016 - November 2020')
+        )
+    ).appendTo('#certificationsContent')
+}
+
+// #Certifications
+
+
+// Skills
+function skillsDiv(workExp) {
+    $('<h3/>').addClass('mb-5').append('Skills').appendTo('#skillsContent');
+
+    // skillsDivSection('');
+    // workExp.content.forEach(item => workExperienceSingle(item));
+}
+
+function skillsDivSection(items) {
+    $('<div/>').addClass(['subheading', 'mb-3']).append('Languages');
+
+    $('<table/>').addClass(['ml-3', 'mb-3']).append(
+        skillsDivSingle('')
+        // workExp.content.forEach(item => workExperienceSingle(item));
+    )
+}
+
+function skillsDivSingle(item) {
+    $('<tr/>').append(
+        $('<td/>').append('<i class="fab fa-java"></i>' + ' ' + 'Java'),
+        $('<td/>').append(score5new())
+    )
+
+    // workExp.content.forEach(item => workExperienceSingle(item));
+}
+
+// #Skills
+
+
+workExperienceDiv('');
+voluntaryExperienceDiv('');
+educationDiv('');
+certificationsDiv('');
+// skillsDiv('');
 
 
 
